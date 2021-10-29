80dc0a24d791ae80ffa43428def3d99253152aa9
Fabian Simon <fabian.simon@inovex.de>
2021-10-29 12:43:54 +0200

    added grid css for example

--- externalServices/left-menu/pages/index.tsx
+++ externalServices/left-menu/pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      Test      
+      Left menu
     </div>
   )
 }
