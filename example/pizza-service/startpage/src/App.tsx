d5f73737e56038860d599415a4339a78f080946c
Fabian Simon <fabian.simon@inovex.de>
2022-03-30 09:14:15 +0200

    wip

--- example/pizza-service/startpage/src/App.tsx
+++ example/pizza-service/startpage/src/App.tsx
@@ -1,11 +1,77 @@
 import { useState } from 'react'
+import styled from 'styled-components'
+
+const Root = styled.div`
+  height: 80vh;
+
+`
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+`
+const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  `
+
+const Item = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  width: 100vh;
+  border-top: 1px solid black;
+
+  background-color: #f5f5f5;
+  :last-child{
+    border-bottom: 1px solid black;
+  }
+  :hover {
+    cursor: pointer;
+    background-color: #e5e5e5;
+  }
+`
 
 function App() {
+
+  const [items, setItems] = useState(['Pizza 1', 'Pizza 2', 'Pizza 3'])
+
     return (
-    <h1>
-      Startpage content
-    </h1>
+      <Root>
+        <Header>
+        <h1>
+          We have the best pizza at net
+        </h1>
+        </Header>
+        <Content>
+          <h2>Order your Favorit Pizza:</h2>
+          <List>
+            {items.map((item, index) => (
+              <Item onClick={() => {
+                
+              }} key={index}>
+                <h3>{item}</h3>
+              </Item>
+            ))}
+          </List>
+        </Content>
+        
+    </Root>
   )
+
 }
 
 export default App
